3ba1fb04ecbc5a470eab09641ff38f5734f3d58e
Paweł Matyszok <pawel.matyszok@gmail.com>
2013-10-15 17:27:54 +0200

    addes asm placeholder library

---
+++ JA-proj/AsmImpl/main.asm
@@ -0,0 +1,34 @@
+.386 
+
+.MODEL FLAT, STDCALL
+
+
+OPTION CASEMAP:NONE
+
+INCLUDE    \masm32\include\windows.inc 
+INCLUDE		\masm32\include\user32.inc
+INCLUDELIB	\masm32\lib\user32.lib
+
+.DATA
+Smth	DB	"Test2", 0
+Result	QWORD	?
+.CODE
+
+DllEntry PROC hInstDLL:HINSTANCE, reason:DWORD, reserved1:DWORD
+
+	mov eax, TRUE 
+	ret
+
+DllEntry ENDP
+
+;-------------------------------------------------------------------------
+; To jest pusta funkcja. Nic nie robi. Wstawi³em j¹ tutaj, aby ci pokazaæ,
+; gdzie nale¿y umieszczaæ w³asne funkcje w bibliotece DLL
+;-------------------------------------------------------------------------
+
+TestFunction PROC
+	invoke MessageBox, NULL, ADDR Smth, ADDR Smth, MB_OK
+	ret
+TestFunction ENDP
+
+END DllEntry 
--- JA-proj/JA-proj/main.cpp
+++ JA-proj/JA-proj/main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-typedef void (*gamma)();
+typedef void (*__stdcall gamma)();
 
 
 
@@ -15,11 +15,20 @@ int main()
 	gamma fun = (gamma)GetProcAddress(lib, "CorrectGamma");
 	if (fun == NULL)
 		return -2;
+	HMODULE asmLib = LoadLibrary("AsmImpl.dll");
+	if (asmLib == NULL)
+		return -3;
+	gamma fun2 = (gamma)GetProcAddress(asmLib, "TestFunction");
+	if (fun2 == NULL)
+		return -4;
 
 	fun();
+	fun2();
 	int ay;
 	cin >> ay;
 	if (lib != NULL)
 		FreeLibrary(lib);
+	if (asmLib != NULL)
+		FreeLibrary(asmLib);
 	return 0;
 }
